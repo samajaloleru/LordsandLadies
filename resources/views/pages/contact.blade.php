dc12045a3e74caf5836432840f851cd3bb29f79f
samajaloleru <samsonajaloleru@gmail.com>
2021-01-27 00:28:01 +0100

    add content to webpages

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -134,8 +134,7 @@
             </div>
             
         </a>
-        
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12286.715636833376!2d-75.59837531200412!3d39.65694025682884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c703f3d191cf13%3A0xf4674106f987fe3a!2s245+Quigley+Blvd+Ste+K%2C+New+Castle%2C+DE+19720%2C+USA!5e0!3m2!1sen!2sua!4v1530266633608" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-        
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d991.0348257521061!2d3.3884922!3d6.5040441!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8cf2a187f34d%3A0x9560675ce4b3078c!2s19%20Olaboye%20Olaleye%20St%2C%20Iwaya%2C%20Lagos!5e0!3m2!1sen!2sng!4v1611703532251!5m2!1sen!2sng" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>                
     </div>
 @endsection
